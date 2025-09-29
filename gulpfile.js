4b96c5422f7a93c056f168de51a3aafe0b13b588
VeronikaSmahula <veronika.smahula.pm.2024@lpnu.ua>
2025-09-29 17:41:12 +0300

    created html file

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const cleanCSS = require("gulp-clean-css");
+const rename = require("gulp-rename");
 const browserSync = require("browser-sync").create();
 
-// Шлях до файлів
+// Шляхи
 const paths = {
   html: "src/html/**/*.html",
   styles: "src/scss/**/*.scss",
@@ -14,13 +16,16 @@ const paths = {
 gulp.task("html", function () {
   return gulp.src(paths.html)
     .pipe(gulp.dest("dist"))
-    .pipe(browserSync.stream());
+    .pipe(browserSync.reload({ stream: true }));
 });
 
-// SCSS → CSS
+// SCSS → CSS (компіляція + мініфікація)
 gulp.task("styles", function () {
   return gulp.src(paths.styles)
     .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("dist/css"))        // версія без мініфікації
+    .pipe(cleanCSS())                    // мінімізуємо CSS
+    .pipe(rename({ suffix: ".min" }))   // додаємо .min
     .pipe(gulp.dest("dist/css"))
     .pipe(browserSync.stream());
 });
@@ -29,22 +34,22 @@ gulp.task("styles", function () {
 gulp.task("scripts", function () {
   return gulp.src(paths.scripts)
     .pipe(gulp.dest("dist/js"))
-    .pipe(browserSync.stream());
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 // Images
 gulp.task("images", function () {
   return gulp.src(paths.images)
     .pipe(gulp.dest("dist/images"))
-    .pipe(browserSync.stream());
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 // BrowserSync + Watcher
 gulp.task("serve", function () {
   browserSync.init({
-    server: {
-      baseDir: "dist"
-    }
+    server: { baseDir: "dist" },
+    notify: false,
+    open: true
   });
 
   gulp.watch(paths.html, gulp.series("html"));
